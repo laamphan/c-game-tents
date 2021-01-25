8d0cd6b13681d3023e77dc1d6bda50acd86a182d
laamphan <tlaamphan@gmail.com>
2021-01-25 02:09:44 +0100

    Update game_aux functions and game.c | extern

--- game_aux.c
+++ game_aux.c
@@ -58,35 +58,17 @@ void game_print(cgame g) {
 }
 
 game game_default(void) {
-  uint nb_tents_row[DEFAULT_SIZE] = {3, 0, 4, 0, 4, 0, 1, 0};
-  uint nb_tents_col[DEFAULT_SIZE] = {4, 0, 1, 2, 1, 1, 2, 1};
-  square squares[DEFAULT_SIZE * DEFAULT_SIZE] = {
-      EMPTY, EMPTY, EMPTY, EMPTY, TREE, TREE, EMPTY, EMPTY,
-      TREE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, TREE,
-      EMPTY, EMPTY, EMPTY, EMPTY, TREE, EMPTY, EMPTY, EMPTY,
-      TREE, EMPTY, EMPTY, EMPTY, EMPTY, TREE, EMPTY, EMPTY,
-      EMPTY, TREE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-      TREE, EMPTY, EMPTY, EMPTY, TREE, EMPTY, TREE, EMPTY,
-      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-      TREE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
-
-  game g = game_new(squares, nb_tents_row, nb_tents_col);
+  extern uint nb_tents_row_default[DEFAULT_SIZE];
+  extern uint nb_tents_col_default[DEFAULT_SIZE];
+  extern square squares_default[DEFAULT_SIZE * DEFAULT_SIZE];
+  game g = game_new(squares_default, nb_tents_row_default, nb_tents_col_default);
   return g;
 }
 
 game game_default_solution(void) {
-  uint nb_tents_row[DEFAULT_SIZE] = {3, 0, 4, 0, 4, 0, 1, 0};
-  uint nb_tents_col[DEFAULT_SIZE] = {4, 0, 1, 2, 1, 1, 2, 1};
-  square squares[DEFAULT_SIZE * DEFAULT_SIZE] = {
-      TENT, GRASS, GRASS, TENT, TREE, TREE, TENT, GRASS,
-      TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, TREE,
-      TENT, GRASS, GRASS, TENT, TREE, TENT, GRASS, TENT,
-      TREE, GRASS, GRASS, GRASS, GRASS, TREE, GRASS, GRASS,
-      TENT, TREE, TENT, GRASS, TENT, GRASS, TENT, GRASS,
-      TREE, GRASS, GRASS, GRASS, TREE, GRASS, TREE, GRASS,
-      TENT, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS,
-      TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS};
-
-  game g = game_new(squares, nb_tents_row, nb_tents_col);
+  extern uint nb_tents_row_default[DEFAULT_SIZE];
+  extern uint nb_tents_col_default[DEFAULT_SIZE];
+  extern square squares_default_solution[DEFAULT_SIZE * DEFAULT_SIZE];
+  game g = game_new(squares_default_solution, nb_tents_row_default, nb_tents_col_default);
   return g;
 }
