807a6ac4c5be0a62be69ac4c67a9f1696754941c
laamphan <tlaamphan@gmail.com>
2021-01-26 00:31:28 +0100

    Update location of Default variables

--- game_aux.c
+++ game_aux.c
@@ -3,6 +3,29 @@
 #include <stdio.h>
 #include "game.h"
 
+uint nb_tents_row_default[DEFAULT_SIZE] = {3, 0, 4, 0, 4, 0, 1, 0};
+uint nb_tents_col_default[DEFAULT_SIZE] = {4, 0, 1, 2, 1, 1, 2, 1};
+
+square squares_default[DEFAULT_SIZE * DEFAULT_SIZE] = {
+    EMPTY, EMPTY, EMPTY, EMPTY, TREE, TREE, EMPTY, EMPTY,
+    TREE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, TREE,
+    EMPTY, EMPTY, EMPTY, EMPTY, TREE, EMPTY, EMPTY, EMPTY,
+    TREE, EMPTY, EMPTY, EMPTY, EMPTY, TREE, EMPTY, EMPTY,
+    EMPTY, TREE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+    TREE, EMPTY, EMPTY, EMPTY, TREE, EMPTY, TREE, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+    TREE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
+
+square squares_default_solution[DEFAULT_SIZE * DEFAULT_SIZE] = {
+    TENT, GRASS, GRASS, TENT, TREE, TREE, TENT, GRASS,
+    TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, TREE,
+    TENT, GRASS, GRASS, TENT, TREE, TENT, GRASS, TENT,
+    TREE, GRASS, GRASS, GRASS, GRASS, TREE, GRASS, GRASS,
+    TENT, TREE, TENT, GRASS, TENT, GRASS, TENT, GRASS,
+    TREE, GRASS, GRASS, GRASS, TREE, GRASS, TREE, GRASS,
+    TENT, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS,
+    TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS};
+
 void game_print(cgame g) {
   assert(g);
   uint nb_rows = DEFAULT_SIZE;
@@ -58,17 +81,11 @@ void game_print(cgame g) {
 }
 
 game game_default(void) {
-  extern uint nb_tents_row_default[DEFAULT_SIZE];
-  extern uint nb_tents_col_default[DEFAULT_SIZE];
-  extern square squares_default[DEFAULT_SIZE * DEFAULT_SIZE];
   game g = game_new(squares_default, nb_tents_row_default, nb_tents_col_default);
   return g;
 }
 
 game game_default_solution(void) {
-  extern uint nb_tents_row_default[DEFAULT_SIZE];
-  extern uint nb_tents_col_default[DEFAULT_SIZE];
-  extern square squares_default_solution[DEFAULT_SIZE * DEFAULT_SIZE];
   game g = game_new(squares_default_solution, nb_tents_row_default, nb_tents_col_default);
   return g;
 }
